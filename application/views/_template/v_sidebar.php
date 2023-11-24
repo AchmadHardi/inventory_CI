9d40254638c69376c041c1c35060c55483cc878c
Mitchell Dirks <pottsed@protonmail.ch>
2023-11-24 08:20:13 +0700

    Commit dev

--- application/views/_template/v_sidebar.php
+++ application/views/_template/v_sidebar.php
@@ -24,18 +24,32 @@
     </li>
 
     <li class="nav-item active">
+        <a class="nav-link" href="<?= base_url('kategori'); ?>">
+            <i class="fa fa-th-large"></i>
+            <span>Kategori</span>
+        </a>
+    </li>
+    <li class="nav-item active">
         <a class="nav-link" href="<?= base_url('barang'); ?>">
             <i class="fa fa-cube"></i>
             <span>Barang</span>
         </a>
     </li>
 
-    <li class="nav-item active">
-        <a class="nav-link" href="<?= base_url('kategori'); ?>">
-            <i class="fa fa-th-large"></i>
-            <span>Kategori</span>
-        </a>
-    </li>
+<li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="false" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Manajemen Stok</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar" style="">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Flow</h6>
+                        <a class="collapse-item" href="<?= base_url(); ?>flow/masuk">Masuk</a>
+                        <a class="collapse-item" href="<?= base_url(); ?>flow/keluar">Keluar</a>
+                        <a class="collapse-item" href="<?= base_url(); ?>flow/riwayat">Riwayat</a>
+                    </div>
+                </div>
+            </li>
 
 </ul>
 <!-- End of Sidebar -->
---
+++ application/views/flow/keluar.php
@@ -0,0 +1,27 @@
+<div class="container-fluid">
+    <div class="col-md-6 col-xs-12">
+        <div class="card mt-2">
+            <div class="card-header">
+                <h3><?= $judul ?></h3>
+            </div>
+            <div class="card-body mt-2">
+
+                <form id="formCreate" action="<?= base_url(); ?>flow/store?flow=keluar" method="post">
+                    <div class="form-group">
+                        <label for="nama_barang">Nama Barang:</label>
+                        <select name="id_barang" class="form-control" required>
+                            <?php foreach ($barang as $b): ?>
+                                <option value="<?= $b->id_barang ?>"><?= $b->nama_barang ?> <span><i>(Sisa stok Saat ini: <?= $b->stok ?>)</i></span></option>
+                            <?php endforeach ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="qty">Stok Keluar </label>
+                        <input type="number" class="form-control" id="qty" name="qty" required min="1">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
